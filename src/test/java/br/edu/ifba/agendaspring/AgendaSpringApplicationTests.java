93237011863b2c327095b72dee761989408c6eef
igorcosta <igorcosta@192.168.1.12>
2021-11-23 19:09:37 -0300

    teste

---
+++ src/test/java/br/edu/ifba/agendaspring/AgendaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifba.agendaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
